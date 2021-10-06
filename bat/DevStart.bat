36f7171d7a37a2f3ae07e3986bb4b913d9c77088
mikersgit <mwroberts@mwroberts.org>
2021-10-06 10:45:31 -0600

    Convenience Windows and VIM scripts
    These were developed to work with Visual code, vim, etc on Javascript

---
+++ bat/DevStart.bat
@@ -0,0 +1,12 @@
+rem start communication tools
+start "WorkBrowser" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 1"
+start "PersonalBrowser" "C:\Program Files\Google\Chrome\Application\chrome.exe" --profile-directory="Profile 2"
+start "Outlook" "c:\Users\20801921\OneDrive - ChampionX\Desktop\OUTLOOK.lnk"
+rem start dev tools
+start "VS2019" "c:\Users\20801921\OneDrive - ChampionX\Desktop\ADMIN Visual Studio 2019.lnk"
+start "PowShell" "c:\Users\20801921\OneDrive - ChampionX\Desktop\ADMIN Windows PowerShell.lnk"
+start "cygwin" "c:\Users\20801921\OneDrive - ChampionX\Desktop\Cygwin64 Terminal.lnk"
+start "Simplemind" "c:\Users\20801921\OneDrive - ChampionX\Desktop\SimpleMind Pro.lnk"
+start "VScode" "c:\Users\20801921\OneDrive - ChampionX\Desktop\Visual Studio Code.lnk"
+start "Gvim" "c:\Users\Public\Desktop\OneDR gVim 8.2.lnk"
+start "GvimSrc" "c:\Users\20801921\OneDrive - ChampionX\Desktop\SRC gVim 8.2.lnk"
