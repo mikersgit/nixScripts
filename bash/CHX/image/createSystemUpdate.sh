814e8d51efecd45488db74af257e3559cf61ebe3
mikersgit <mwroberts@mwroberts.org>
2026-04-09 14:56:36 -0600

    Add files via upload

---
+++ bash/CHX/image/createSystemUpdate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#######################
+# ChampionX 2022
+# create SystemUpdate tar file from mounted sdcard image.
+#######################
+img=$1
+mnt=$2
+#TOOLDIR=/build/image
+TOOLDIR=/localBuild/image
+BLDTOOL=${TOOLDIR}/imgBuild.sh
+
+if [ $# -lt 2 ] ; then
+	i=$( ls -1 sd*[xz,img] |grep -v fresh|head -1)
+
+	echo "${0##*/} <sdcard image> <mount point>"
+	echo "${0##*/} ${i} /mnt/sdcard"
+	exit 1
+fi
+${BLDTOOL} -i $img -b $mnt -m
+
+img=$(echo $img | sed 's/.xz$//')
+ver=$(echo $img | sed -e 's/sdcard-//' -e 's/.img//')
+tarDest="/build/SystemUpdate-${ver}.tar"
+cd ${mnt}
+echo -e "\n\t============ Creating $tarDest from $mnt ============"
+time tar -cpf ${tarDest}  * &
+tarpid=$!
+renice --priority -10 $(pgrep tar)
+wait ${tarpid}
+cd ${TOOLDIR}
+${BLDTOOL} -i $img -b $mnt -u
