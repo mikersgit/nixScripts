3f4a6b5a923bd08dbaef9a0f10d29fa756e4f783
mwr <mwroberts@mwroberts.org>
2026-05-20 17:33:32 -0600

    remove devops token

---
+++ bash/CHX/mikersbin/gitSparseCheckout.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#Script for rootfs to only checkout one image dir either "current"
+#or "winchester"
+#default is "current"
+#Also checkout OS Exodus BBB_kernel and TBone repos
+#
+if [[ ${#} -lt 1 ]]
+then
+	echo "USAGE: ${0##*/} current|winchester"
+	exit 1
+else
+	rfssubtree=${1}
+fi
+
+source TOKEN.src
+
+repos=(rootfs Exodus OS TBone BBB_Kernel)
+repoPaths=(rootfs ExodusCapps OS TBone BBB_Kernel)
+repoSparse=(sparse sparse sparse sparse sparse)
+repoSubtree=(${rfssubtree} Clibs/libRIB2 hmi.qt5 flash/tools src/kernel4/drivers/gpio)
+azureBaseUrl="https://${USER}:${TOKEN}@spiritnps.visualstudio.com"
+echo $azureBaseUrl
+#
+# initialize and pull git repos, checkout sparse if indicated
+#
+function initGitDir() {
+	local frepo=$1
+	local frepoPath=$2
+	local cpwd=${PWD}
+	local sparse=$3
+	local subtree=$4
+	echo "========================="
+	echo "== ${frepo} ${sparse} ========="
+	echo "========================="
+	if mkdir ${frepo} 2>/dev/null ;then
+		:
+	else
+		echo "dir ${frepo} already exists"
+	fi
+	cd ${frepo}
+	if test -d .git
+	then
+		echo "Already a git repo, no need to initialize"
+		echo "Attempting 'pull'"
+		git pull origin master
+	else
+		git init
+		git remote add -f origin ${frepoPath}
+		git config credential.helper store
+		if test -n ${sparse} ;then
+			git config core.sparseCheckout true
+			echo "${subtree}/" > .git/info/sparse-checkout
+			echo "#########################"
+			echo "## ${subtree} #########"
+			echo "#########################"
+		fi
+		git pull origin master
+	fi
+	cd ${cpwd}
+}
+
+git config  --global user.name michael.roberts
+
+echo "clone/update repos ${repos[@]}"
+read -p "Continue? [N]|Y " ans
+i=0
+if [[ ${ans} = [yY] ]]
+then
+	if test -e ${HOME}/.git-credentials ;then
+		cat ${HOME}/.git-credentials
+		echo "==============="
+	fi
+	if ls *[Cc]red* >/dev/null 2>&1 ;then
+		echo "Any credential files of the form <repo>[Cc]red"
+		cat *[Cc]red*
+	fi
+	for r in ${repos[@]}
+	{
+		fullpath=${azureBaseUrl}/${repoPaths[$i]}/_git/${repos[$i]}
+		sparse=${repoSparse[$i]}
+		subtree=${repoSubtree[$i]}
+		initGitDir ${r} ${fullpath} ${sparse} ${subtree}
+		((i+=1))
+	}
+	echo "May need to do additional git config like:"
+	echo "git push --set-upstream origin master"
+	echo "git branch --set-upstream-to=origin/master master"
+else
+	echo "No cloning of ${repos[@]}"
+fi
+]}
+	{
+		fullpath=${azureBaseUrl}/${repoPaths[$i]}/_git/${repos[$i]}
+		sparse=${repoSparse[$i]}
+		subtree=${repoSubtree[$i]}
+		initGitDir ${r} ${fullpath} ${sparse} ${subtree}
+		read -p"continue? y|n " cans
+		if [[ ${cans} = [nN] ]]
+		then
+			break
+		fi
+		((i+=1))
+	}
+	echo "May need to do additional git config like:"
+	echo "git push --set-upstream origin master"
+	echo "git branch --set-upstream-to=origin/master master"
+else
+	echo "No cloning of ${repos[@]}"
+fi
